28ed1f43c1d512d7d8151ecb586bd4f34f6b3ad6
interceptor43 <teo292005@gmail.com>
2024-04-12 09:17:26 +0200

    Ajout circle.c

---
+++ Examen/circle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+double P(double r) {
+    double pi = 3.14159;
+    return 2 * pi * r;
+}
+
+double S(double r) {
+    double pi = 3.14159;
+    return pi * pow(r, 2);
+}
+
+int main() {
+    double rayon;
+    printf("Entrez la valeur du rayon : ");
+    scanf("%lf", &rayon);
+    double perimetre = P(rayon);
+    double surface = S(rayon);
+    printf("Le périmètre du cercle est : %.2lf\n", perimetre);
+    printf("La surface du disque est : %.2lf\n", surface);
+
+    return 0;
+}
