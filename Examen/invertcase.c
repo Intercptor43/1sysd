53a05516bf70a6ec88a29ccc9c88bc59ab8a019e
interceptor43 <teo292005@gmail.com>
2024-04-12 10:06:48 +0200

    Ajout invertcase.c

--- Examen/invertcase.c
+++ Examen/invertcase.c
@@ -17,7 +17,7 @@ void invertcase(char *s) {
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Usage: %s \"string\"\n", argv[0]);
+        printf("Utilisation: %s \"string\"\n", argv[0]);
         return 1;
     }
     invertcase(argv[1]);
